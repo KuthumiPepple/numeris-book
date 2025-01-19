d27259d2a8002de0daf32e287ba650e50fb7e229
Kuthumi Pepple <kuthumipepple@gmail.com>
2025-01-19 08:54:00 +0100

    chore: load config with viper

--- main.go
+++ main.go
@@ -7,22 +7,23 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/kuthumipepple/numeris-book/api"
 	"github.com/kuthumipepple/numeris-book/db"
-)
-
-const (
-	dbSource          = "postgresql://root:secret@localhost:5432/bank?sslmode=disable"
-	httpServerAddress = "0.0.0.0:8080"
+	"github.com/kuthumipepple/numeris-book/util"
 )
 
 func main() {
-	connPool, err := pgxpool.New(context.Background(), dbSource)
+	config, err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatal("cannot connect to db")
+		log.Fatal("cannot load config:", err)
+	}
+
+	connPool, err := pgxpool.New(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
 	}
 
 	store := db.NewStore(connPool)
 	apiServer := api.NewServer(store)
-	err = apiServer.Start(httpServerAddress)
+	err = apiServer.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server")
 	}
